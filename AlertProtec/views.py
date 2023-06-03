1d28aaf0374c0ca9b78f63dc119f0e7430796af5
rqueaArequipa <ruben.quea@tecsup.edu.pe>
2023-06-03 02:29:45 -0500

    Test de FIlters

--- AlertProtec/views.py
+++ AlertProtec/views.py
@@ -9,16 +9,16 @@ class MachineryViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
     serializer_class = MachinerySerializer
     filter_backends =[DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = '__all__'
-    search_fields = '__all__'
+    filterset_fields = ['id', 'type_machinery']
+    search_fields = ['id', 'type_machinery']
     
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = PersonSerializer
     filter_backends =[DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = '__all__'
-    search_fields = '__all__'
+    filterset_fields = ['id', 'name']
+    search_fields = ['id', 'name']
     
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -33,5 +33,5 @@ class AlertViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
     serializer_class = AlertSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = '__all__'
-    search_fields = '__all__'
+    filterset_fields = ['id', 'status']
+    search_fields = ['id', 'status']
