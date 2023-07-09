960408c28c4698b6c24c5a4c1b5f977fe04e59e0
rqueaArequipa <ruben.quea@tecsup.edu.pe>
2023-07-09 15:50:12 -0500

    Test-End

--- AlertProtec/models.py
+++ AlertProtec/models.py
@@ -17,17 +17,16 @@ class Machinery(models.Model):
         return self.type_machinery
 
 class Person(models.Model):
-    name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     dni = models.CharField(max_length=20)
     number = models.CharField(max_length=15)
     email = models.EmailField(unique=True)
     date_birth = models.DateField()
     address = models.CharField(max_length=50)
     certifications = models.CharField(max_length=200)
-    rol = models.IntegerField(default=0)
-    machinery = models.ForeignKey(Machinery, on_delete=models.CASCADE, blank=True)
-    
+    machinery = models.ForeignKey(Machinery, on_delete=models.CASCADE, blank=True, null=True)
+
     def __str__(self):
         return self.name
     
