2eed8fe64bfb2c387fae7773c75de1b36c29e225
rqueaArequipa <ruben.quea@tecsup.edu.pe>
2023-07-10 03:11:51 -0500

    Test - Templates

--- Alert/views.py
+++ Alert/views.py
@@ -3,7 +3,7 @@ from django.views import View
 from rest_framework.response import Response
 from django.http import HttpResponse
 
-class IndexView(View):
-    def get(self,request):
-        #context = {'mensaje':'<Button>Click Aqui</Button>'}
-        return HttpResponse('<Button style="background: black;"><a href="/api" style="text-decoration: none; color: white;">Click Aqui</a></Button>')
+from django.shortcuts import render
+
+def IndexView(request):
+    return render(request, 'index.html')
